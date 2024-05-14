8ee811baa6f9c1d4cb4d2ea6b712254bed6d7508
APIVerve Team <hello@apiverve.com>
2024-05-14 15:43:51 -0500

    Bug fixes and Improvements

---
+++ timezonelookupQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class timezonelookupQueryOptions {
+public string timezone { get; set; }
+}
+
+}
