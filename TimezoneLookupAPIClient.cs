5d8381a0e4fd32573ea64ef9e2c7b309c7cc2443
APIVerve Team <hello@apiverve.com>
2024-08-20 11:11:34 -0500

    Bug Fixes

--- TimezoneLookupAPIClient.cs
+++ TimezoneLookupAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, timezonelookupQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, TimezoneLookupQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(timezonelookupQueryOptions options = null)
+        public ResponseObj Execute(TimezoneLookupQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(timezonelookupQueryOptions options)
+        private string constructURL(TimezoneLookupQueryOptions options)
         {
             string url = _apiEndpoint;
 
